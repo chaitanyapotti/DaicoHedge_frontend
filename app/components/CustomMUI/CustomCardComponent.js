a038ae2ed441a0a1946982ff9842c1ca11aefe1f
aayush2252 <aayushgupta1991@gmail.com>
2018-12-08 01:21:24 +0800

    css and helpers added

---
+++ app/components/CustomMUI/CustomCardComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+
+export const CustomCard = props => <Card>{props.children}</Card>;
--- app/components/Home.js
+++ app/components/Home.js
@@ -2,7 +2,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
-import styles from './Home.css';
 
 type Props = {};
 
@@ -11,9 +10,8 @@ export default class Home extends Component<Props> {
 
   render() {
     return (
-      <div className={styles.container} data-tid="container">
+      <div>
         <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
       </div>
     );
   }
