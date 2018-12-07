096e41981286a4934b1ad1dec5abdb5b70c76848
Vinay Bagul <vinay@yun.buzz>
2018-12-08 03:13:36 +0530

    new

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,6 +2,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
+import "../static/css/app.global.css";
+import {Button} from "@material-ui/core";
 
 type Props = {};
 
@@ -10,7 +12,8 @@ export default class Home extends Component<Props> {
 
   render() {
     return (
-      <div>
+      <div className="testname">
+        <Button>Hello</Button>
         <h2>Home</h2>
       </div>
     );
